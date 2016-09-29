36b721565805ef9368404b451215ef184bab4e7a
Wtm <micromaomao@gmail.com>
2016-09-29 22:29:11 +0800

    Get stuff from argv.

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-Open `hack.js`:
-
-    /*
-      On line 19, replace the long string with your *cookie* on https://app.myimaths.com.
-      On line 20, replace the URL with the URL of the question page.
-      On line 30 and 31, replace the mark with the desired mark. It can't be greater
-        than the question total mark or this won't work.
-    */
-
-Than run `./hack.js`. If it output 200 and some other stuff, than you're all good!
+`./hack.js`
 
 [The code](hack.js) is very short and you can read it through easily.
 
--- hack.js
+++ hack.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 
-/*
-  On line 20, replace the long string with your *cookie* on https://app.myimaths.com.
-  On line 21, replace the URL with the URL of the question page.
-  On line 31 and 32, replace the mark with the desired mark. It can't be greater
-    than the question total mark or this won't work.
-  Run this program. If it output 200 and some other stuff, than you're all good!
-*/
+const args = process.argv.slice(2)
+if (args.length < 3) {
+  console.log('Usage: ./hack.js <Cookie on https://app.myimaths.com> <Url of the question page> <Desired score for question #1> <for question #2>...')
+  process.exit(1)
+}
+const scores = args.slice(2).map(score => {
+  let num = parseInt(score)
+  if (num < 0) {
+    console.log('Score must be >= 0; At score ' + score)
+    process.exit(2)
+    return null
+  }
+  if (!Number.isSafeInteger(num)) {
+    console.log('Score must be a integer that make sense; At score ' + score)
+    process.exit(2)
+    return null
+  }
+  return num
+})
 
 const request = require('request')
 const qs = require('querystring')
@@ -17,8 +29,17 @@ const metaHeader = {
   'Origin': 'https://app.myimaths.com',
   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)',
     // Simplifying this more results in browser upgrade redirect.
-  'Cookie': 'myimaths_session_id=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;',
-  'Referer': 'https://app.myimaths.com/xxxx-xxxxxxxx/xxxxxx',
+  'Cookie': args[0],
+  'Referer': args[1],
+}
+
+let postObj = {
+  testID: 2004, // Changing this don't seems to do anything, but the server requires it.
+}
+
+for (let q = 0; q < scores.length; q ++) {
+  let qno = q + 1
+  postObj[`q${qno}score`] = scores[q]
 }
 
 request({
@@ -26,18 +47,19 @@ request({
   method: 'post',
   headers: metaHeader,
   followRedirect: false,
-  body: qs.stringify({
-    testID: 2004, // Changing this don't seems to do anything, but the server requires it.
-    q1score: 10,
-    q2score: 15,
-  })
+  body: qs.stringify(postObj)
 }, (err, icm, res) => {
   if (err) {
     console.error(err)
+    console.log('Sorry, something goes wrong.')
+    process.exit(3)
+    return
   }
-  console.log(icm.statusCode)
-  console.log(res)
-  process.nextTick(() => {
-    process.exit(icm.statusCode === 200 ? 0 : 1)
-  })
+  if (icm.statusCode !== 200) {
+    console.log("Sorry, This don't work anymore. Fire a GitHub issue now!")
+    process.exit(3)
+    return
+  }
+  console.log('Success! Check out your results page.')
+  process.exit(0)
 })
