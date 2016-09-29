5272adc60b6490d8d5c796160f862c89e8fb619a
Wtm <micromaomao@gmail.com>
2016-09-29 22:10:33 +0800

    Add comment

--- hack.js
+++ hack.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 /*
-  On line 19, replace the long string with your *cookie* on https://app.myimaths.com.
-  On line 20, replace the URL with the URL of the question page.
-  On line 30 and 31, replace the mark with the desired mark. It can't be greater
+  On line 20, replace the long string with your *cookie* on https://app.myimaths.com.
+  On line 21, replace the URL with the URL of the question page.
+  On line 31 and 32, replace the mark with the desired mark. It can't be greater
     than the question total mark or this won't work.
   Run this program. If it output 200 and some other stuff, than you're all good!
 */
@@ -16,6 +16,7 @@ const metaHeader = {
   'Accept-Language': 'en,zh;q=0.8',
   'Origin': 'https://app.myimaths.com',
   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)',
+    // Simplifying this more results in browser upgrade redirect.
   'Cookie': 'myimaths_session_id=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;',
   'Referer': 'https://app.myimaths.com/xxxx-xxxxxxxx/xxxxxx',
 }
