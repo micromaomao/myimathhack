6a57d27277e019851f305368b4b4ced9af6bc022
Mao Wtm <micromaomao@gmail.com>
2016-12-29 19:45:35 +0800

    Add randomness

--- calcScore.hs
+++ calcScore.hs
@@ -1,17 +1,26 @@
 import System.Environment
+import System.Random
 
 calc :: (Integral t) => t -> [t] -> [t]
 calc precentage nums = map (\num -> truncate ((fromIntegral num) * (fromIntegral precentage) / (fromIntegral 100))) nums
+
+randAlter :: (RandomGen g, Integral t) => g -> [t] -> [Int]
+randAlter rdgen ([]) = []
+randAlter rdgen (it:itleft) = (fromIntegral it + nSam):(randAlter nrdgen itleft) where (nSam, nrdgen) = randomR (-2, 2) rdgen
 
 processInput :: Int -> IO ()
 processInput precentage = do
   inp <- getLine
   let nums = [read numStr | numStr <- words inp ] :: [Int]
-  let res = unwords ( map show ( calc precentage nums ) )
+  let calcRes = calc precentage nums
+  rdgen <- newStdGen
+  let alteredRes = randAlter rdgen calcRes
+  let res = unwords ( map show alteredRes )
   putStrLn res
   processInput precentage
   return ()
 
 main = do
   args <- getArgs
-  if length args /= 1 then error "./calcScore precentage" else processInput ( read ( head args ) )
+  let precentage = read ( head args )
+  if length args /= 1 then error "./calcScore precentage" else processInput precentage
